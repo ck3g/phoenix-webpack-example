cb57357cfaf0d7a1b8809fe4e1d744c33b6c0efa
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-05-15 22:21:25 +0200

    Configure react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,5 @@
 import css from "../css/app.css"
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
 // Import local files
@@ -12,3 +8,10 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Jumbotron from './components/Jumbotron';
+
+ReactDOM.render(<Jumbotron />, document.getElementById("react-app"));
---
+++ assets/js/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron">
+      <h2>Welcome to Phoenix with Webpack and React</h2>
+      <p className="lead">A productive web framework that<br />does not compromise speed and maintainability.</p>
+    </div>
+  )
+};
+
+export default Jumbotron;
