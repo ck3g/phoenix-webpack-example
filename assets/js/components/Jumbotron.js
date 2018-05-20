ca10c537e519f4d09314a55910d887a451d08723
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-05-20 19:22:33 +0200

    Paint title orange

--- assets/js/components/Jumbotron.js
+++ assets/js/components/Jumbotron.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './Jumbotron.css';
 
 const Jumbotron = () => {
   return (
     <div className="jumbotron">
-      <h2>Welcome to Phoenix with Webpack and React</h2>
+      <h2 class="colorful-text">Welcome to Phoenix with Webpack and React</h2>
       <p className="lead">
         A productive web framework that<br />
         does not compromise speed and maintainability.
