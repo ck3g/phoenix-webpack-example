7b9e5567c0fd339286acd46f5e6674f33a39c634
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-05-20 13:58:45 +0200

    Add link to the article

--- assets/js/components/Jumbotron.js
+++ assets/js/components/Jumbotron.js
@@ -4,7 +4,15 @@ const Jumbotron = () => {
   return (
     <div className="jumbotron">
       <h2>Welcome to Phoenix with Webpack and React</h2>
-      <p className="lead">A productive web framework that<br />does not compromise speed and maintainability.</p>
+      <p className="lead">
+        A productive web framework that<br />
+        does not compromise speed and maintainability.
+      </p>
+      <p className="lead">
+        <a href="http://whatdidilearn.info/2018/05/20/how-to-use-webpack-and-react-with-phoenix-1-3.html">
+          Read the full article
+        </a>
+      </p>
     </div>
   )
 };
